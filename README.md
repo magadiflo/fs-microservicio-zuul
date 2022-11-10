85a5d9982d89e11311b892f47ca4519e820c0cfe
Martín <magadiflo@gmail.com>
2022-11-10 12:10:13 -0500

    Proyecto base - Con dependencias base: Eureka Client y Spring Web

---
+++ README.md
@@ -0,0 +1,4 @@
+# Microservicios Spring Cloud Eureka & Angular Full-stack
+
+## Sección 3: Backend: Eureka Server Registrando microservicios
+19. Creando y configurando servidor Zuul Gateway
---
+++ src/main/java/com/magadiflo/zuul/FsMicroservicioZuulApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMicroservicioZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMicroservicioZuulApplication.class, args);
+	}
+
+}
